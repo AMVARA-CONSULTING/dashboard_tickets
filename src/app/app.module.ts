1df3b1445871f2f924351d60694123ca311587fc
Alex Barba <leparadoxhd@gmail.com>
2018-11-10 15:16:34 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/ux/header/header.component';
+import { FooterComponent } from './components/ux/footer/footer.component';
+import { MenuIconComponent } from './components/ux/header/menu-icon/menu-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuIconComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/ux/header/menu-icon/menu-icon.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'cism-menu-icon',
+  templateUrl: './menu-icon.component.html',
+  styleUrls: ['./menu-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MenuIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
