2f8c3d75b853b2863650efda189d8d2e1f1c74e8
xavier <hello.xavier@amvara.de>
2018-11-14 11:52:53 +0100

    .

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfigService } from '@services/config.service';
 
 @Component({
   selector: 'cism-root',
@@ -7,5 +9,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  constructor(private translate:TranslateService, public config:ConfigService){
+    translate.setDefaultLang('en')
+    translate.use(localStorage.getItem('lang') || config.config.language)
+
+  }
   title = 'cism';
 }
