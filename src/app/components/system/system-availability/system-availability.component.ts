af8fe59e2dd435d3a854f2f9664fc0d32be9c04a
Alex Barba <leparadoxhdv@gmail.com>
2020-09-23 14:08:28 +0200

    Fix logs of system

--- src/app/components/system/system-availability/system-availability.component.ts
+++ src/app/components/system/system-availability/system-availability.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ChangeDetectionStrategy, Host, OnDestroy } from '@angular/core';
-import { DataService } from '@services/data.service';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { SAPercents, SAViewType, Config } from '@other/interfaces';
 import { SystemGraphicHolderComponent } from '@components/system/system-graphic-holder/system-graphic-holder.component';
@@ -17,9 +16,9 @@ import { TicketsState } from '@states/tickets.state';
 })
 export class SystemAvailabilityComponent implements OnInit, OnDestroy {
 
-  ngOnDestroy = () => this.subs.unsubscribe()
+  ngOnDestroy = () => this.subs.unsubscribe();
 
-  subs = new SubSink()
+  subs = new SubSink();
 
   @Select(TicketsState.Section1) tickets$: Observable<any[]>
 
@@ -28,102 +27,114 @@ export class SystemAvailabilityComponent implements OnInit, OnDestroy {
     private _tools: ToolsService,
     @Host() private _holder: SystemGraphicHolderComponent
   ) {
-    this.config = this._store.selectSnapshot<Config>(store => store.config)
-    this._holder.titles.next([this.config.system.titles.S1])
+    this.config = this._store.selectSnapshot<Config>(store => store.config);
+    this._holder.titles.next([this.config.system.titles.S1]);
   }
 
-  config: Config
-  
-  percents = new BehaviorSubject<SAPercents>(null)
+  config: Config;
 
-  SystemAvailabilityRows = new BehaviorSubject<any[]>([])
+  percents = new BehaviorSubject<SAPercents>(null);
 
-  view = new BehaviorSubject<SAViewType>('overview')
+  SystemAvailabilityRows = new BehaviorSubject<any[]>([]);
+
+  view = new BehaviorSubject<SAViewType>('overview');
 
   ngOnInit() {
     // Filter system rows with System Availability section
     this.subs.sink = this.tickets$.subscribe(SARows => {
       // Extract today's %
-      let today = 0
+      let today = 0;
       try {
-        const date = format(new Date(), this.config.system.S1.formatDate)
-        today = this._tools.formatPercent(SARows.filter(row => row[1] == date)[0][2])
+        const date = format(new Date(), this.config.system.S1.formatDate);
+        today = this._tools.formatPercent(SARows.filter(row => row[1] == date)[0][2]);
       } catch (err) {
-        console.log('System Availability', 'Processing', 'Percent not found for today')
-        console.error(err)
+        console.log('=======================================================');
+        console.log('System Availability', 'Processing', 'Percent not found for today. Check you system report!!!');
+        console.log('=======================================================');
+        // console.error(err)
       }
       // Extract previous day %
-      let yesterday = 0
+      let yesterday = 0;
       try {
-        let date: any = subDays(new Date(), 1)
-        date = format(date, this.config.system.S1.formatDate)
-        yesterday = this._tools.formatPercent(SARows.filter(row => row[1] == date)[0][2])
+        let date: any = subDays(new Date(), 1);
+        date = format(date, this.config.system.S1.formatDate);
+        yesterday = this._tools.formatPercent(SARows.filter(row => row[1] == date)[0][2]);
       } catch (err) {
-        console.log('System Availability', 'Processing', 'Percent not found for yesterday')
-        console.error(err)
+        console.log('=======================================================');
+        console.log('System Availability', 'Processing', 'Percent not found for yesterday. Check you system report!!!');
+        console.log('=======================================================');
+        // console.error(err)
       }
       // Extract previous week %
-      let prev_week = 0
+      let prev_week = 0;
       try {
-        let week: any = subWeeks(new Date(), 1)
-        week = getWeek(week)
+        let week: any = subWeeks(new Date(), 1);
+        week = getWeek(week);
         const rows = SARows.filter(row => {
-          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date())
-          return getWeek(date) == week
-        })
-        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0)
-        prev_week = this._tools.formatPercent(sum / rows.length)
+          const date: any = parse(row[1], this.config.system.S1.formatDate, new Date());
+          return getWeek(date) == week;
+        });
+        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0);
+        prev_week = this._tools.formatPercent(sum / rows.length);
       } catch (err) {
-        console.log('System Availability', 'Processing', 'Percent not found for previous week')
-        console.error(err)
+        console.log('=======================================================');
+        console.log('System Availability', 'Processing', 'Percent not found for previous week. Check you system report!!!');
+        console.log('=======================================================');
+        // console.error(err)
       }
       // Extract current week %
-      let act_week = 0
+      let act_week = 0;
       try {
-        const week = getWeek(new Date())
+        const week = getWeek(new Date());
         // @ts-ignore
         const rows = SARows.filter(row => {
-          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date())
-          date = getWeek(date)
-          return date == week
-        })
-        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0)
-        act_week = this._tools.formatPercent(sum / rows.length)
+          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date());
+          date = getWeek(date);
+          return date == week;
+        });
+        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0);
+        act_week = this._tools.formatPercent(sum / rows.length);
       } catch (err) {
-        console.log('System Availability', 'Processing', 'Percent not found for actual week')
-        console.error(err)
+        console.log('=======================================================');
+        console.log('System Availability', 'Processing', 'Percent not found for actual week. Check you system report!!!');
+        console.log('=======================================================');
+        // console.error(err)
       }
       // Extract previous month %
-      let prev_month = 0
+      let prev_month = 0;
       try {
-        const month = getMonth(subMonths(new Date(), 1))
+        const month = getMonth(subMonths(new Date(), 1));
         // @ts-ignore
         const rows = SARows.filter(row => {
-          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date())
-          date = getMonth(date)
-          return date == month
-        })
-        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0)
-        prev_month = this._tools.formatPercent(sum / rows.length)
+          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date());
+          date = getMonth(date);
+          return date == month;
+        });
+        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0);
+        prev_month = this._tools.formatPercent(sum / rows.length);
       } catch (err) {
-        console.log('System Availability', 'Processing', 'Percent not found for previous month')
-        console.error(err)
+        console.log('=======================================================');
+        console.log('System Availability', 'Processing', 'Percent not found for previous month. Check you system report!!!');
+        console.log('=======================================================');
+        // console.error(err)
       }
       // Extract current month %
-      let act_month = 0
+      let act_month = 0;
       try {
-        const month = getMonth(new Date())
+        const month = getMonth(new Date());
         // @ts-ignore
         const rows = SARows.filter(row => {
-          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date())
-          date = getMonth(date)
-          return date == month
-        })
-        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0)
-        act_month = this._tools.formatPercent(sum / rows.length)
+          let date: any = parse(row[1], this.config.system.S1.formatDate, new Date());
+          date = getMonth(date);
+          return date == month;
+        });
+        const sum = rows.map(r => r[2]).reduce((r, a) => r + a, 0);
+        act_month = this._tools.formatPercent(sum / rows.length);
       } catch (err) {
-        console.log('System Availability', 'Processing', 'Percent not found for actual month')
-        console.error(err)
+        console.log('=======================================================');
+        console.log('System Availability', 'Processing', 'Percent not found for actual month. Check you system report!!!');
+        console.log('=======================================================');
+        // console.error(err)
       }
       // Return obtained stats
       const result = {
@@ -135,11 +146,11 @@ export class SystemAvailabilityComponent implements OnInit, OnDestroy {
         yesterday_up: today > yesterday ? 'up' : 'down',
         week_up: act_week > prev_week ? 'up' : 'down',
         month_up: act_month > prev_month ? 'up' : 'down'
-      }
+      };
       // Render DOM
-      this.percents.next(result)
-      this.SystemAvailabilityRows.next(result.rows)
-    })
+      this.percents.next(result);
+      this.SystemAvailabilityRows.next(result.rows);
+    });
   }
 
 }
